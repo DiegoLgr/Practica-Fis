6fff58b55575f5dfe04c262b10a5e050c8b83c0d
sergio <siorata@gmail.com>
2019-05-12 18:29:39 +0200

    SIORATa
    PENE

--- Implementacion/src/main/java/es/upm/fis2019/Usuario.java
+++ Implementacion/src/main/java/es/upm/fis2019/Usuario.java
@@ -35,10 +35,11 @@ public class Usuario implements IUsuario, IRecuperador {
 
     @Override
     public Publicacion[] GetPublicacionesUsuario(Object PrimeraFecha) {
-        PublicacionTexto[] publicaciones = new PublicacionTexto[3];
+        PublicacionTexto[] publicaciones = new PublicacionTexto[6];
         publicaciones[0] = new PublicacionTexto("1", 1, 1, "Primera publicación");
         publicaciones[1] = new PublicacionTexto("2", 4, 1, "Como usar maven");
         publicaciones[2] = new PublicacionTexto("3", 100000, 1, "Por qué usar Git en vez de SVN");
+        publicaciones[5] = new PublicacionTexto("5", 100000, 1, "PENE");
         return publicaciones;
     }
 
