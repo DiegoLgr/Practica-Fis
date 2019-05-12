457375809fdc066d5dd49a7c402cbb02f00f2bdf
Carlos <cdlopezs02@gmail.com>
2019-05-12 19:24:21 +0200

    commit pal push

--- Implementacion/src/main/java/es/upm/fis2019/Usuario.java
+++ Implementacion/src/main/java/es/upm/fis2019/Usuario.java
@@ -36,20 +36,13 @@ public class Usuario implements IUsuario, IRecuperador {
     @Override
     public Publicacion[] GetPublicacionesUsuario(Object PrimeraFecha) {
         PublicacionTexto[] publicaciones = new PublicacionTexto[6];
-        //hola pene
 
-
-
-
-
-
-        //pene
         publicaciones[0] = new PublicacionTexto("1", 1, 1, "Primera publicación");
         publicaciones[1] = new PublicacionTexto("2", 4, 1, "Como usar maven");
         publicaciones[2] = new PublicacionTexto("3", 100000, 1, "Por qué usar Git en vez de SVN");
-        publicaciones[3] = new PublicacionTexto("4", 1, 1000, "Arriba Espania");
-        publicaciones[5] = new PublicacionTexto("5", 100000, 1, "PENE");
-        publicaciones[4] = new PublicacionTexto("3", 100000, 1, "vez de SVN");
+        publicaciones[3] = new PublicacionTexto("4", 1, 1000, "hola Espania");
+        publicaciones[5] = new PublicacionTexto("5", 100000, 1, "PENlnkhlkuiugE");
+        publicaciones[4] = new PublicacionTexto("3", 100000, 1, "vez asdfghkjvbl hkvkjgyhjhj de SVN");
         return publicaciones;
     }
 
