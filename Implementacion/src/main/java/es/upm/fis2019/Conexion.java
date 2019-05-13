31e40324365c5b8cc7c018f7d219954d448f8602
Carlos <cdlopezs02@gmail.com>
2019-05-14 01:22:44 +0200

    Conexion Creado como singleton

--- Implementacion/src/main/java/es/upm/fis2019/Conexion.java
+++ Implementacion/src/main/java/es/upm/fis2019/Conexion.java
@@ -5,10 +5,27 @@ public class Conexion implements IEjecutador {
 	private String User;
 	private String Mode;
 	private String Pssw;
+	private static Conexion conexion;
+
+	//Constructor Privado
+	private Conexion(){
+		this.Path="aaa";
+	}
+
+	//Singleton
+	public static Conexion getInstance(){
+		if(conexion==null)
+			conexion=new Conexion();
+
+		return conexion;
+	}
+
 	public void ejecutar(String peticion){
 
 	};
 	public Boolean conectar(){
 	return true;
 	};
+
+
 }
