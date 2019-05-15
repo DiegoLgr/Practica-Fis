2600ba480e1d6a36a6abb0a23743c5d0a75492f1
Diego Ripoll <diego.lgrip@gmai.com>
2019-05-15 16:38:28 +0200

    Interfaz de comentarios

--- Implementacion/src/main/java/es/upm/fis2019/Comentario.java
+++ Implementacion/src/main/java/es/upm/fis2019/Comentario.java
@@ -1,4 +1,35 @@
 package es.upm.fis2019;
 
-public class Comentario {
+public class Comentario implements IComentario {
+    String id;
+    String texto;
+    String fecha;
+    String respuesta;
+
+    public Comentario(String id, String texto, String fecha, String respuesta){
+        this.id = id;
+        this.texto = texto;
+        this.fecha = fecha;
+        this.respuesta = respuesta;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String getTexto() {
+        return texto;
+    }
+
+    @Override
+    public String getFecha() {
+        return fecha;
+    }
+
+    @Override
+    public String getRespuesta() {
+        return respuesta;
+    }
 }
