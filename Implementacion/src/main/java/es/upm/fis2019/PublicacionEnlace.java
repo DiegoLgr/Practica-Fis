de827ac45d031f54803516f11514fbf61d3b5af5
Diego Ripoll <diego.lgrip@gmai.com>
2019-05-16 10:27:43 +0200

    Hecha la parte de conseguir las publicacinoes propias

--- Implementacion/src/main/java/es/upm/fis2019/PublicacionEnlace.java
+++ Implementacion/src/main/java/es/upm/fis2019/PublicacionEnlace.java
@@ -1,4 +1,21 @@
 package es.upm.fis2019;
 
-public class PublicacionEnlace {
+public class PublicacionEnlace extends Publicacion {
+    String contenido;
+    public PublicacionEnlace(String id, int likes, int dislikes, String contenido) {
+        super(id, likes, dislikes);
+        this.contenido = contenido;
+    }
+
+
+
+    public String getContenido() {
+        String respresentaciónContenido = this.contenido + "    Likes: " +this.likes + "     Dislikes: " + this.dislikes;
+        return respresentaciónContenido;
+    }
+
+    @Override
+    public Comentario[] consultarComentarios() {
+        return new Comentario[0];
+    }
 }
