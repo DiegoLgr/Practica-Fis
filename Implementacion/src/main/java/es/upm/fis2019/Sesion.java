5118276b29f0c38a7238cf182199861e65d6f113
Fran <fran-701@hotmail.com>
2019-05-18 12:28:50 +0200

    Ahora si esta bien modificado sesion

--- Implementacion/src/main/java/es/upm/fis2019/Sesion.java
+++ Implementacion/src/main/java/es/upm/fis2019/Sesion.java
@@ -3,22 +3,14 @@ package es.upm.fis2019;
 public class Sesion {
     private IUsuario usuario;
     private static Sesion sesion;
-    public Sesion(){
-        usuario= new Usuario("a" ,"a","a");
-    }
 
     public Sesion(IUsuario user){
         usuario = user;
     }
 
-    public Sesion getInstance(IUsuario usuario){
-        if(sesion==null)
-            sesion= new Sesion(usuario);
-        return this;
+    public static Sesion getInstance(){
+        return sesion;
     }
-
-
-
 
     public void setUsuario(Usuario usuario){
         this.usuario = usuario;
