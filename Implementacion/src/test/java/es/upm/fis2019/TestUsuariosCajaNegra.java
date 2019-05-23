ff0d771ab209c57cd3faed09784d0ce2f1c2c5c6
Miguel <m99hormigo@gmail.com>
2019-05-23 18:45:42 +0200

    Tests

--- Implementacion/src/test/java/es/upm/fis2019/TestUsuariosCajaNegra.java
+++ Implementacion/src/test/java/es/upm/fis2019/TestUsuariosCajaNegra.java
@@ -4,14 +4,32 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 public class TestUsuariosCajaNegra {
+    List<IPublicacion> publicaciones;
     Usuario usr;
+    private IEjecutador accesobd;
+    Texto texto;
+    PublicacionTexto publicacion;
+
     @Before
     public void setUp() throws Exception {
-       usr= new Usuario("a","aaa","aaaa");
+        usr= new Usuario("franciscohc","franhc@gmail.com","arribaespaña69");
+        texto = new Texto("España es el mejor país del mundo.");
+        publicacion = new PublicacionTexto("España", 100, 0, texto);
+        publicaciones = new ArrayList<IPublicacion>();
 
+        accesobd=Conexion.getInstance();
+        accesobd.conectar();
+        String query1 = "insert into usuario (alias,correo,contraseña)\n" +
+                //"values(\"franciscohc\",\"franhc@gmail.com\",\"arribaespaña69\");";
+        //accesobd.ejecutar(query1);
+        accesobd.desconectar();
     }
 
     @After
@@ -22,12 +40,12 @@ public class TestUsuariosCajaNegra {
 
     @Test
     public void getAlias() {
-
+        assertEquals("franciscohc", usr.getAlias());
     }
 
     @Test
     public void getCorreo() {
-        assertEquals("aaa",usr.getCorreo());
+        assertEquals("franhc@gmail.com",usr.getCorreo());
     }
 
     @Test
@@ -40,10 +58,40 @@ public class TestUsuariosCajaNegra {
 
     @Test
     public void getPublicacionesUsuario() {
+        String query1="select *\n" +
+                "from publicacion\n" +
+                "where id in (select p_id\n" +
+                "from publica pu, usuario u\n" +
+                "where u.alias = pu.u_id\n" +
+                "and u.alias = '"+usr.getAlias()+"')";
+        assertEquals(publicaciones, usr.GetPublicacionesUsuario("2019-05-12"));
     }
 
     @Test
     public void publicar() {
+        usr.publicar(publicacion);
+        accesobd.conectar();
+
+        String query1="select * from publicacion where id = '"+publicacion.getId()+"';";
+        ResultSet rs = accesobd.ejecutarQuery(query1);
+        try {
+            String aux=rs.getString(6);
+            String id=rs.getString(1);
+            int likes=rs.getInt(2);
+            int dislikes=rs.getInt(3);
+            String fecha=rs.getString(4);
+            String contenido=rs.getString(5);
+
+            assertEquals(id, publicacion.getId());
+            assertEquals(likes, publicacion.getLikes());
+            assertEquals(dislikes, publicacion.getDislikes());
+            //assertEquals(fecha, publicacion.getFecha());
+            assertEquals(contenido, publicacion.getContenido());
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
+        accesobd.desconectar();
     }
 
     @Test
@@ -60,9 +108,13 @@ public class TestUsuariosCajaNegra {
 
     @Test
     public void verificarCredenciales() {
-
     }
 
-
-
+    public static void main(String[] args) throws Exception {
+        TestUsuariosCajaNegra test =new TestUsuariosCajaNegra();
+        test.setUp();
+        test.getAlias();
+        test.getCorreo();
+        test.publicar();
+    }
 }
